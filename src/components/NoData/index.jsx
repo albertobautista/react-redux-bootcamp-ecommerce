5d05b67c2e8b925f5e8dfe8d648ca99453b3fe9c
Daniel Alberto Chávez Bautista <33468378+albertobautista@users.noreply.github.com>
2022-07-10 18:11:45 -0500

    create NoData component

---
+++ src/components/NoData/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoData = ({ message }) => {
+  return (
+    <div className="border d-flex justify-content-center">
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default NoData;
