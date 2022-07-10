dda2c9b196af753984649ab4d5dbe9e3c5d792db
Daniel Alberto Chávez Bautista <33468378+albertobautista@users.noreply.github.com>
2022-07-10 18:12:11 -0500

    add Cart and NoData components to CartPage

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import Cart from "../components/Cart";
+import NoData from "../components/NoData";
+import { CartContext } from "../contexts/CartContext";
 
-export const Cart = () => {
+export const CartPage = () => {
+  const { cartItems, cartTotalPrice, cartItemsQuantity } =
+    useContext(CartContext);
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="mt-5">
+      {cartItems.length > 0 ? (
+        <Cart
+          cartItems={cartItems}
+          cartTotalPrice={cartTotalPrice}
+          cartItemsQuantity={cartItemsQuantity}
+        />
+      ) : (
+        <NoData message="Carrito vacío" />
+      )}
+    </div>
+  );
+};
