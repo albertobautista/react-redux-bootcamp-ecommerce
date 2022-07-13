48b6145f0337cfa80e17b67e4226f1e18f5812c2
Daniel Alberto Chávez Bautista <33468378+albertobautista@users.noreply.github.com>
2022-07-12 20:36:28 -0500

    add context provider to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import GlobalStyle from "./styles/globalStyles";
 import { AppRouter } from "./routes/AppRouter";
 import CartContextProvider from "./contexts/CartContext";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <CartContextProvider>
-        <GlobalStyle />
-        <AppRouter />
-      </CartContextProvider>
+      <AuthContextProvider>
+        <CartContextProvider>
+          <GlobalStyle />
+          <AppRouter />
+        </CartContextProvider>
+      </AuthContextProvider>
     </>
   );
 }
