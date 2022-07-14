79f9f8f8295f6fe1d7a8125d55f868c9eaa9321f
Daniel Alberto Chávez Bautista <33468378+albertobautista@users.noreply.github.com>
2022-07-14 17:36:56 -0500

    Add products slice, selectors and thunks

---
+++ src/redux/slices/products/selectors.js
@@ -0,0 +1,3 @@
+export const isLoadingProducts = (state) => state.products.isLoading;
+export const productslData = (state) => state.products.data;
+export const productsError = (state) => state.products.error;
