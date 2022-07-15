538f97a9146259e7e2599b8584c7953f73daf256
Daniel Alberto Chávez Bautista <33468378+albertobautista@users.noreply.github.com>
2022-07-15 12:50:03 -0500

    change context to redux in ProductsSummary component

--- src/components/ProductsSummary/index.jsx
+++ src/components/ProductsSummary/index.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { currencyFormat } from "../../utils/currency";
+import {
+  cartTotalPriceSelector,
+  cartTotalSelector,
+} from "../../redux/slices/cart/selectors";
+import { cleanCart } from "../../redux/slices/cart/cartSlice";
 
 export const ProductsSummary = ({ cartTotalPrice, cartItemsQuantity }) => {
+  const total = useSelector(cartTotalSelector);
+  const totalPrice = useSelector(cartTotalPriceSelector);
+  const dispatch = useDispatch();
+
+  const handleCleanCart = () => {
+    dispatch(cleanCart());
+  };
+
   return (
     <div className="card">
       <div className="card-body">
         <dl className="dlist-align ">
           <dd className="text-right text-dark b ml-3">
-            <strong>{cartItemsQuantity}</strong> product
-            {cartItemsQuantity > 1 ? "s" : ""}
+            <strong>{total}</strong> product
+            {total > 1 ? "s" : ""}
           </dd>
         </dl>
         <dl className="dlist-align ">
           <dt>Total: </dt>
           <dd className="text-right text-dark b ml-3">
-            <strong>{currencyFormat(cartTotalPrice)}</strong>
+            <strong>{currencyFormat(totalPrice)}</strong>
           </dd>
         </dl>
 
@@ -28,7 +42,7 @@ export const ProductsSummary = ({ cartTotalPrice, cartItemsQuantity }) => {
             Checkout
           </Link>
           <button
-            onClick={() => {}}
+            onClick={handleCleanCart}
             className="btn btn-out btn-danger btn-square btn-main mt-2"
           >
             Clean cart
