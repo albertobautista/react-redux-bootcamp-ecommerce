a3e3765c03b33b20ea59e5bd31a91437a5fac32d
Daniel Alberto Chávez Bautista <33468378+albertobautista@users.noreply.github.com>
2022-07-15 21:37:20 -0500

    add validation to only call fetch products if products array is empty

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,29 +1,45 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
 import {
   isLoadingProducts,
   productsError,
   productslData,
 } from "../../redux/slices/products/selectors";
 import { fetchProducts } from "../../redux/slices/products/thunks";
+import NoData from "../NoData";
 
 import ProductItem from "../ProductItem";
 
 const ProductList = () => {
   const dispatch = useDispatch();
+
   const products = useSelector(productslData);
   const isLoading = useSelector(isLoadingProducts);
   const error = useSelector(productsError);
 
   useEffect(() => {
+    if (products.length > 0) return;
     dispatch(fetchProducts());
   }, []);
 
   if (isLoading) {
-    return <div>Cargando...</div>;
+    return (
+      <div className="container">
+        <div className="row d-flex justify-content-md-start justify-content-center">
+          <NoData message="Loading" />
+        </div>
+      </div>
+    );
   }
   if (error.isError) {
-    return <div>{error.message}</div>;
+    return (
+      <div className="container">
+        <div className="row d-flex justify-content-md-start justify-content-center">
+          <NoData message={error.message} />
+        </div>
+      </div>
+    );
   }
   return (
     <div className="container">
